a9a32cc793202a04b7cdad15b9bbef5877ff50e3
V-keerththanan <keerththananphy@gmail.com>
2022-10-25 15:49:12 +0530

    developed appp

--- App.py
+++ App.py
@@ -10,11 +10,21 @@ def sayHi():
 
 @app.route("/uploaded",methods = ['POST','GET'])
 def getimage():
-    if request.method=='POST':
-        print(request.files.get('filename'))
-        array_image=tf.keras.preprocessing.image.load_img(request.form.get('filename'))
-        print(array_image)
-        return "lol"
-
+ if request.method=='POST':
+  img = request.files['my_image']
+  img_path = "static/" + img.filename	
+  img.save(img_path)
+  p = predict_label(img_path).tolist()
+  ind=p[0].index(max(p[0]))
+  
+ return f"{ind}"
+        
+def predict_label(img_path):
+  model=tf.keras.models.load_model("Myresnet.h5")
+  i = tf.keras.preprocessing.image.load_img(img_path, target_size=(680,480))
+  i = tf.keras.preprocessing.image.img_to_array(i)/255.0
+  i = i.reshape(1, 680,480,3)
+  p = model.predict(i)
+  return p
 if __name__=='__main__':
     app.run(debug=True)
